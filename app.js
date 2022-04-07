e9e2afd02dcc8947e77c7f016dc6908fda5c013a
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-07 22:26:08 +0300

    gitignore created!

---
+++ app.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=express();
+
+app.get('/',(req ,res)=>{
+    res.send('Hellooooo');
+})
+
+const port=3000;
+
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı.`)
+})
