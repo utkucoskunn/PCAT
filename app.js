b75521e6d78be6126eb342f22405a4afc750023a
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-08 23:59:45 +0300

    Models created!

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set("view engine","ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -21,6 +23,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
     res.render('add')
+})
+
+app.post('/photos', (req, res) => {
+    console.log(req.body);
+    res.redirect('/')
 })
 
 
