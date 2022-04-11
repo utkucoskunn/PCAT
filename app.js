c3fe0ffe88cdcae27fda4f1dc284501295c882c9
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-11 12:31:41 +0300

    İmage path added!

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const ejs=require('ejs');
-const path=require('path');
-const Photo=require('./models/Photo');
+const fileUpload = require('express-fileupload');
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
+const Photo = require('./models/Photo');
 
 
 const app = express();
 
 //connect DB
-mongoose.connect('mongodb://localhost/pcat-db',{
+mongoose.connect('mongodb://localhost/pcat-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 //TEMPLATE ENGINE
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req, res) => {
-    const photos= await Photo.find({})
-    res.render('index',{
+    const photos = await Photo.find({}).sort('-dateCreated')
+    res.render('index', {
         photos
     })
 })
 
 app.get('/photos/:id', async (req, res) => {
-    const photo=await Photo.findById(req.params.id)
-    res.render('photo',{
-    photo
+    const photo = await Photo.findById(req.params.id)
+    res.render('photo', {
+        photo
     })
 });
 
@@ -44,9 +47,24 @@ app.get('/add', (req, res) => {
     res.render('add')
 })
 
-app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
+app.post('/photos', (req, res) => {
+
+    const uploadDir = 'public/uploads';
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    let uploadedImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadedImage.name,
+        })
+        res.redirect('/');
+    });
 });
 
 
