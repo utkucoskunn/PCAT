6fe733b638ab7b28016846d68e756e8db0c20983
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-11 15:07:04 +0300

    delete function added!

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/pcat-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    //useFindAndModify:false
+
 });
 
 //TEMPLATE ENGINE
@@ -25,7 +27,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method',{
+    methods:['POST','GET']
+}));
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -86,6 +90,13 @@ app.put('/photos/:id', async (req, res) => {
     res.redirect(`/photos/${req.params.id}`)
 })
 
+app.delete('/photos/:id', async (req,res)=>{
+    const photo=await Photo.findOne({_id:req.params.id})
+    let deletedImage=__dirname+'/public'+photo.image;
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndRemove(req.params.id)
+    res.redirect('/');
+})
 
 const port = 3000;
 
